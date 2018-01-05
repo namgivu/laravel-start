cd24bbceb21be4b9bfc10aef892c7ffa90aec899
namgivu <namgivu@gmail.com>
2018-01-05 15:28:28 +0700

    task @ route controller, view

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,57 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//region /task handlers
+
+/*
+ * read GET /tasks
+ */
+Route::get('/tasks', function (Request $request) {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+
+/*
+ * create POST /tasks
+ */
+Route::post('/tasks', function (Request $request) {
+    //params validation
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/tasks')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    //create The Task
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/tasks');
+});
+
+
+/*
+ * delete DELETE /tasks/{id}
+ */
+Route::delete('/tasks/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/tasks');
+});
+
+//endregion /task handlers
